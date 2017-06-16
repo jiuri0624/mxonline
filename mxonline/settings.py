03a93d1ec99c3e68ff3ad03a8f2c0edd8006a1ff
xu zhang <xu zhang>
2017-06-16 23:20:01 +0800

    增加model

--- mxonline/settings.py
+++ mxonline/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'courses',
+    'organization',
 ]
 
 AUTH_USER_MODEL = 'users.UserProfile'
--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+from datetime import datetime
 
 
 class UserProfile(AbstractUser):
@@ -20,3 +21,26 @@ class UserProfile(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class EmailVerifyRecord(models.Model):
+    code = models.CharField(max_length=20, verbose_name=u'验证码')
+    email = models.EmailField(max_length=50, verbose_name=u'邮箱')
+    send_type = models.CharField(choices=(('register', u'注册'), ('forget', u'找回密码')), max_length=10)
+    send_time = models.DateTimeField(default = datetime.now)
+
+    class Meta:
+        verbose_name = u'验证码'
+        verbose_name_plural = verbose_name
+
+
+class Banner(models.Model):
+    title = models.CharField(max_length=100, verbose_name=u'标题')
+    image = models.ImageField(upload_to='banner/%Y/m', verbose_name=u'轮播图')
+    url = models.URLField(max_length=200, verbose_name=u'访问地址')
+    index = models.IntegerField(verbose_name=u'排序')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
+
+    class Meta:
+        verbose_name = u'轮播图'
+        verbose_name_plural = verbose_name
