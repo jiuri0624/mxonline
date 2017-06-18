435386d59b116087076c79fc09a33b0f197c3784
xu zhang <xu zhang>
2017-06-18 23:00:39 +0800

    完成model注册

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -22,6 +22,9 @@ class Course(models.Model):
         verbose_name = u'课程'
         verbose_name_plural = verbose_name
 
+    def __str__(self):
+        return self.name
+
 
 class Lesson(models.Model):
     course = models.ForeignKey(Course, verbose_name=u'课程')
--- apps/users/models.py
+++ apps/users/models.py
@@ -25,8 +25,8 @@ class UserProfile(AbstractUser):
 class EmailVerifyRecord(models.Model):
     code = models.CharField(max_length=20, verbose_name=u'验证码')
     email = models.EmailField(max_length=50, verbose_name=u'邮箱')
-    send_type = models.CharField(choices=(('register', u'注册'), ('forget', u'找回密码')), max_length=10)
-    send_time = models.DateTimeField(default = datetime.now)
+    send_type = models.CharField(choices=(('register', u'注册'), ('forget', u'找回密码')), max_length=10,verbose_name=u'验证码类型')
+    send_time = models.DateTimeField(default = datetime.now, verbose_name=u'发送时间')
 
     class Meta:
         verbose_name = u'验证码'
